dc81fdae1fa6794af876394500f66ba989b515a5
Ben N <93800156+benedictnurs@users.noreply.github.com>
2023-10-28 15:16:32 -0700

    Added multiple maps

--- components/coords.py
+++ components/coords.py
@@ -1,5 +1,8 @@
+import streamlit as st
+import pydeck as pdk
+
 #Plots the coords needed to map
-def latitude(area, data_sorted, position, default):
+def coords(area, data_sorted, position, default):
     if area == "All":
         areaLat = default
     else:
@@ -12,3 +15,43 @@ def latitude(area, data_sorted, position, default):
 
 
 
+#Creates the map
+def hex_map(lat, lon, data_sorted, scale, result):
+    st.pydeck_chart(pdk.Deck(
+        map_style=None,
+        initial_view_state=pdk.ViewState(
+
+
+        #Lets the lat and lon of the map so it remaps every time you switch area!!!!! Either entire LA or specific selected area
+        latitude=lat,
+        longitude=lon,
+
+        #Adjusts the zoom important!!!!
+        zoom=11,
+        pitch=50,
+    ),
+    layers=[
+
+
+        #Plots the hexagons important!!!!!
+        pdk.Layer(
+
+           'HexagonLayer',
+
+           #Chooses the data that is getting ploted
+           data=data_sorted,
+
+           #Selects the lon and lats columns in the data 
+           get_position='[LON, LAT]',
+           radius=200,
+
+           #Scales the hexagons depending on the amount of rows given
+           elevation_scale = scale,
+
+           #Elevation of the hexagons
+           elevation_range=[0, result],
+           pickable=True,
+           extruded=True,
+        )
+    ],
+))
--- main.py
+++ main.py
@@ -15,8 +15,10 @@ from components.elevation import *
 #Configs the Pages
 st.set_page_config(layout="wide", page_title="LA Crime Data")
 
+
 #Sets the page title 
-st.title("Crime Data in LA")
+st.title("Crime Data in Los Angeles")
+
 
 #Creates dataframe with all the data loaded in
 df = data(815882)
@@ -32,61 +34,52 @@ with st.sidebar:
 
 
 #Creates a subheader to show which area you selected 
-st.subheader(area)
 
 
 #If area is All then returns 80K rows if not return the specific area only.
 data_sorted = data_select(df,area)[0] 
 scale = data_select(df,area)[1]
 
+data_hollywood = data_select(df,"Hollywood")[0]
+scale_hollywood = data_select(df,"Hollywood")[1]
+
+data_central = data_select(df,"Central")[0]
+scale_central = data_select(df,"Central")[1]
 
 #Calls the function from coords.py to use as variables for the map
-lat = latitude(area, data_sorted,"LAT",34.052235)
-lon = latitude(area, data_sorted,"LON",-118.243683)
+lat = coords(area, data_sorted,"LAT",34.052235)
+lon = coords(area, data_sorted,"LON",-118.243683)
 
+lat_hollywood = coords("Hollywood", data_hollywood,"LAT",34.052235)
+lon_hollywood = coords("Hollywood", data_hollywood,"LON",-118.243683)
+
+lat_central = coords("Central", data_central,"LAT",34.052235)
+lon_central = coords("Central", data_central,"LON",-118.243683)
 
 #Filters the data by dates in order to plot within the range also edits the previous variable of data_sorted
 data_sorted = data_filter_date(data_sorted, start, end)
 
+data_hollywood = data_filter_date(data_hollywood, start, end)
+
+data_central = data_filter_date(data_central, start, end)
+
 
 #Makes the scale 5% of the total data unless it is over 50000 then set automatic value of 2500 to prevent overcrowding
 result = elevation(data_sorted)
+result_hollywood = elevation(data_hollywood)
+result_central = elevation(data_central)
 
 
-#Creates the map
-st.pydeck_chart(pdk.Deck(
-    map_style=None,
-    initial_view_state=pdk.ViewState(
+col1, col2, col3 = st.columns(3)
 
-        #Lets the lat and lon of the map so it remaps every time you switch area!!!!! Either entire LA or specific selected area
-        latitude=lat,
-        longitude=lon,
+with col1:
+    st.subheader(area)
+    map1 = hex_map(lat, lon, data_sorted, scale, result)
 
-        #Adjusts the zoom important!!!!
-        zoom=11,
-        pitch=50,
-    ),
-    layers=[
+with col2:
+    st.subheader("Hollywood")
+    map2 = hex_map(lat_hollywood, lon_hollywood, data_hollywood, scale_hollywood, result_hollywood)
 
-        #Plots the hexagons important!!!!!
-        pdk.Layer(
-
-           'HexagonLayer',
-
-           #Chooses the data that is getting ploted
-           data=data_sorted,
-
-           #Selects the lon and lats columns in the data 
-           get_position='[LON, LAT]',
-           radius=200,
-
-           #Scales the hexagons depending on the amount of rows given
-           elevation_scale = scale,
-
-           #Elevation of the hexagons
-           elevation_range=[0, result],
-           pickable=True,
-           extruded=True,
-        )
-    ],
-))
+with col3:
+    st.subheader("Central LA")
+    map3 = hex_map(lat_central, lon_central, data_central, scale_central, result_central)
